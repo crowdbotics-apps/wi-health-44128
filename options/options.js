064fc4388d543336f0700d9d06b18c25d6604d46
Crowdbotics <team@crowdbotics.com>
2023-10-24 14:45:53 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wi-health-44128",
+  url: "https://wi-health-44128.botics.co",
+  api: "https://wi-health-44128.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
